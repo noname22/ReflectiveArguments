932c92e5ddf7bdcd1509d235c2950d5a880b7381
Fredrik Hultin <noname@nurd.se>
2021-09-06 15:39:25 -0300

    added publish.ps1

---
+++ publish.ps1
@@ -0,0 +1,3 @@
+﻿Select-Xml -Path .\ReflectiveArguments\ReflectiveArguments.csproj -XPath '/Project/PropertyGroup/Version' | ForEach-Object { $version = $_.Node.InnerXML }
+dotnet pack -c Release
+dotnet nuget push ReflectiveArguments\bin\Release\ReflectiveArguments.$version.nupkg --api-key $args[0] --source https://api.nuget.org/v3/index.json
