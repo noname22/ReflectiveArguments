9a20a07230e12e2d12dae4e44e2218e59897a126
Fredrik Hultin <noname@nurd.se>
2024-10-23 14:44:38 +0200

    Add test for including argument descriptions in help text

---
+++ ReflectiveArguments.Tests/HelpTests.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using ReflectiveArguments.Tests.TestTypes;
+using System;
+using Xunit;
+
+namespace ReflectiveArguments.Tests;
+public class HelpTests
+{
+    [Fact]
+    public void GetHelp_WithMethodContainingDescriptionAttribute_ShouldUseDescriptionInHelp()
+    {
+        var cmd = Command.FromMethod(OtherStaticClass.StaticMethodWithArgumentDescriptions);
+        var help = new Help(cmd);
+        var helpText = string.Join(Environment.NewLine, help.GetHelp());
+        helpText.Should().Contain("This is an argument");
+    }
+}
--- ReflectiveArguments.Tests/TestTypes/OtherStaticClass.cs
+++ ReflectiveArguments.Tests/TestTypes/OtherStaticClass.cs
@@ -1,7 +1,14 @@
-﻿namespace ReflectiveArguments.Tests.TestTypes;
+﻿using System.ComponentModel;
+
+namespace ReflectiveArguments.Tests.TestTypes;
 public static class OtherStaticClass
 {
     public static void StaticMethod(string someArgument)
+    {
+        SomeArgument = someArgument;
+    }
+
+    public static void StaticMethodWithArgumentDescriptions([Description("This is an argument")] string someArgument)
     {
         SomeArgument = someArgument;
     }
